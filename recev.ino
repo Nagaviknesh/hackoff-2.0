5d4065cfdfe8de8cb47d7ba34823d47c21f9fb60
Nagaviknesh <47518909+Nagaviknesh@users.noreply.github.com>
2019-12-15 09:50:41 +0530

    Add files via upload

---
+++ recev.ino
@@ -0,0 +1,52 @@
+#include <RH_ASK.h> 
+#include <SPI.h> // Not actualy used but needed to compile
+#include <LiquidCrystal.h>
+#include<String.h>
+RH_ASK driver;
+const int rs = 12, en = 10, d4 =5,d5 =4,d6 = 3,d7 =2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7); 
+static char message[100];
+int i=0;
+uint8_t buf[15];
+    uint8_t buflen = sizeof(buf);
+ 
+void setup()
+{
+   
+    Serial.begin(9600);	// Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+      lcd.begin(16,2);
+}
+
+void loop(){
+
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+     // Message with a good checksum received, dump it.
+      Serial.print("Message: ");
+      Serial.println((char*)buf);
+      lcd.setCursor(0,0); 
+      lcd.println((char*)buf);     
+      SendMessage((char*)buf);
+     
+    }
+   
+}
+void SendMessage(char *NIRP)
+{
+
+  Serial.println("AT+CMGF=1");
+  delay(1000);
+  Serial.println("AT+CMGS=\"+919080752236\"\r");   
+  delay(1000);
+   if(strcmp(NIRP ,"Critical")==0)
+   {
+  Serial.println("Emergency:");
+  Serial.println("The Person needs your Help!!!");
+  Serial.println((char)26);
+  }
+ // Serial.println((char)26);
+  delay(5000);
+}
+
---
+++ trans.ino
@@ -0,0 +1,72 @@
+
+const int flexPin1 = A0; 
+const int flexPin2 = A1;
+const int ledPin = 13; 
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600);	  // Debugging only
+    pinMode(ledPin,OUTPUT);
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+  int flexValue1,flexValue2;
+  int flexval1,flexval2;
+  const char *msg;
+  flexValue1 = analogRead(flexPin1);
+  flexValue2 = analogRead(flexPin2);
+  flexval1=flexValue1/7;
+  flexval2=flexValue2/7;
+  Serial.print("sensor 1: ");
+  Serial.println(flexval1);
+  Serial.print("sensor 2: ");
+  Serial.println(flexval2);
+  if(flexval1<115
+ 
+  && flexval2<120)
+  {
+    msg = "Normal!!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(5000);
+  }
+  else if(flexval1>120 && flexval1<130 && flexval2<120)
+  {
+    msg = "!!Food!!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(5000);
+  }
+  else if(flexval1<120 && flexval2>120 && flexval2<130)
+  {
+    msg = "water!!!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(5000);
+  }
+  else if(flexval1>120 && flexval1<135 && flexval2>120 && flexval2<135)
+  {
+    msg = "Medicine";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(5000);
+  }
+  else if(flexval1>135 && flexval2>135)
+  {
+    msg = "Critical";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1000);
+  }
+  else 
+  {
+  }
+}
+
